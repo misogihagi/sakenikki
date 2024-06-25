7649d706ae3532657dd2fb67ec4eb0532dbe8e25
misogihagi <47350059+misogihagi@users.noreply.github.com>
2024-06-26 02:33:07 +0900

    update running hono server script for sqlite

--- server/run.ts
+++ server/run.ts
@@ -1,4 +1,18 @@
 import { serve } from '@hono/node-server';
-import app from './src';
+import * as fs from 'fs';
+import path from 'path';
+import makeApp from './src';
+import migrate from './src/db/sqlite/migrate';
 
-serve(app);
+const DEFAULT_OUTPUT_DIR = './data';
+
+function prepare(outputDir:string) {
+  if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true });
+}
+function main(outputDir = DEFAULT_OUTPUT_DIR) {
+  prepare(outputDir);
+  const dbPath = path.join(outputDir, 'sqlite.db');
+  if (!fs.existsSync(dbPath)) migrate(dbPath);
+  serve(makeApp(outputDir));
+}
+main();
